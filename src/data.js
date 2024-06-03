68ad9e6790f5a80e72df0ae6b7e59fd081e26223
Mazhar1857 <ali.mazhar1857@gmail.com>
2024-06-03 07:42:01 +0530

    add separate file for data

---
+++ src/data.js
@@ -0,0 +1,19 @@
+export const faq = [
+    {
+        question: 'What is Frontend and how will it help me?',
+        answer: 'Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It\'s suitable for all levels and ideal for portfolio building.'
+    },
+    {
+        question: 'Is Frontend Mentor free?',
+        answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus eligendi explicabo magnam sint debitis maxime, vitae minima mollitia maiores beatae omnis magni ea aspernatur? Quod?'
+    },
+    {
+        question: 'Can I use Frontend Mentor project inmy portfolio?',
+        answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus eligendi explicabo magnam sint debitis maxime, vitae minima mollitia maiores beatae omnis magni ea aspernatur? Quod?'
+    },
+    {
+        question: 'How can I get help if I\'m stuck on a challenge?',
+        answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus eligendi explicabo magnam sint debitis maxime, vitae minima mollitia maiores beatae omnis magni ea aspernatur? Quod?'
+    },
+
+]
